0762c35b83892190a33b7cf349997fdff5a4541c
kiorky <kiorky@cryptelium.net>
2012-02-12 17:41:33 +0100

    - Let plone viewable even without the diazo theme unapplied:   public and columns css are not overidden but then, not cookable anymore.   Thus we can remove them from being fetched inside the diazo theme. [kiorky] - Do not override the globalnav viewlet to be a full diazo template [kiorky] - Add 'last' class on globalnav last element to be 1140 compliant {kiorky]

--- plonetheme/responsive1140/static/js/theme.js
+++ plonetheme/responsive1140/static/js/theme.js
@@ -1,3 +1,8 @@
 function initializeplonethemeresponsive1140(){
+  /* put last class on global nav */
+  tabs = $('#portal-globalnav li') ;
+  if (tabs.length>0) {
+    $(tabs[tabs.length-1]).addClass('last');
+  }
 }
 jQuery(initializeplonethemeresponsive1140);
